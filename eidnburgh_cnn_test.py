f32120616a4287251f86e9b84cb91118e2d07720
Lars_Surface <larsgudjons@gmail.com>
2023-08-03 14:44:42 +0200

    Moved plotting functions to model_plot_utils.py. Handle Early stopping in separate function.

--- eidnburgh_cnn_test.py
+++ eidnburgh_cnn_test.py
@@ -1,6 +1,5 @@
 import copy
 import os.path
-import time
 from datetime import datetime
 
 import numpy as np
@@ -8,21 +7,17 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
-import cv2
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-from CNN_Models import CNNModel, UNetModel, CNNModelWithDropout
+from CNN_Models import CNNModel
+from model_plot_utils import calc_average_loss_completly_black, calc_average_loss_completly_white, \
+    plot_sample_reconstructions, plot_single_reconstruction, plot_loss
 
 LOSS_SCALE_FACTOR = 1000
 VOLTAGE_VECTOR_LENGTH = 896
 OUT_SIZE = 64
 
 
-# VOLTAGE_VECTOR_LENGTH = 104
-# OUT_SIZE = 64
-
-# Step 2: Prepare the dataset (assuming you have custom dataset in numpy arrays)
 class CustomDataset(data.Dataset):
     def __init__(self, voltage_data, image_data):
         self.voltage_data = voltage_data
@@ -41,137 +36,23 @@ class CustomDataset(data.Dataset):
         return voltage, image
 
 
-def calc_average_loss_completly_black(image_data_tensor, criterion):
-    """
-    To check if the network is learning anything at all, we can check the average loss of the network when the input is
-    completly black. If the network is learning anything, the average loss should be high.
-    (Check if criterion is suitable for this)
-    :param image_data_tensor:
-    :param voltage_data_tensor:
-    :param criterion:
-    :return:
-    """
-    # Display all reconstructed images
-    average_loss = 0
-    for img in image_data_tensor:
-        output = np.zeros((OUT_SIZE, OUT_SIZE))
-        loss = round(criterion(torch.tensor(output), img.view(OUT_SIZE, OUT_SIZE)).item(), 4) * LOSS_SCALE_FACTOR
-        average_loss += loss
-    average_loss /= len(image_data_tensor)  # easier to read
-    print(f"Average loss black: {average_loss}")
-    return average_loss
-
-
-def calc_average_loss_completly_white(image_data_tensor, criterion):
-    # Display all reconstructed images
-    average_loss = 0
-    for img in image_data_tensor:
-        output = np.ones((OUT_SIZE, OUT_SIZE))
-        loss = round(criterion(torch.tensor(output), img.view(OUT_SIZE, OUT_SIZE)).item(), 4) * LOSS_SCALE_FACTOR
-        average_loss += loss
-    average_loss /= len(image_data_tensor)  # easier to read
-    print(f"Average loss white: {average_loss}")
-    return average_loss
-
-
-def plot_sample_reconstructions(image_data_tensor, voltage_data_tensor, model, criterion, num_images=40,
-                                save_path=""):
-    global output
-    # Display all reconstructed images
-    average_loss = 0
-    # select random images
-    random_indices = np.random.randint(0, len(image_data_tensor), num_images)
-    for i in random_indices:
-        img = image_data_tensor[i]
-        img_numpy = img.view(OUT_SIZE, OUT_SIZE).detach().numpy()
-        volt = voltage_data_tensor[i]
-        output = model(volt)
-        output = output.view(OUT_SIZE, OUT_SIZE).detach().numpy()
-        cv2.imshow("Reconstructed Image", cv2.resize(output, (256, 256)))
-        cv2.imshow("Original Image", cv2.resize(img_numpy, (256, 256)))
-        # print loss for each image
-        a = torch.tensor(output)
-        b = img.view(OUT_SIZE, OUT_SIZE)
-        loss = round(criterion(a, b).item(), 4)
-        loss = loss * LOSS_SCALE_FACTOR  # easier to read
-        average_loss += loss
-        cv2.waitKey(1)
-        print(f"Loss: {loss}")
-        # plot comparison with matplotlib
-        plt.subplot(1, 2, 1)
-        plt.imshow(output)
-        plt.title(f"Loss: {loss}")
-        plt.subplot(1, 2, 2)
-        plt.imshow(img_numpy)
-        plt.title("Original")
-        # add colorbar
-        plt.colorbar()
-        if save_path != "":
-            plt.savefig(os.path.join(save_path, f"reconstruction_{i}.png"))
-        plt.show()
-        time.sleep(0.1)
-    return average_loss / num_images
-
-
-def plot_single_reconstruction(model, voltage_data, title = "Reconstructed image", original_image:np.array=None):
-    """
-    Plots a single reconstruction using the model
-    :param model:
-    :param voltage_data:
-    :return:
-    """
-    if type(voltage_data) == np.ndarray:
-        voltage_data = torch.tensor(voltage_data, dtype=torch.float32)
-    elif type(voltage_data) == torch.Tensor:
-        # if dtype is not float32, convert it
-        if voltage_data.dtype != torch.float32:
-            voltage_data = voltage_data.type(torch.float32)
+def handle_early_stopping():
+    global best_val_loss, counter, best_model
+    if val_loss < best_val_loss:  # Early stopping
+        best_val_loss = val_loss
+        counter = 0
+        best_model = copy.deepcopy(model)
     else:
-        raise TypeError(
-            f"voltage_data_tensor must be either a numpy array or a torch tensor but is {type(voltage_data)}")
-    start = time.time()
-    output = model(voltage_data)
-    stop = time.time()
-    print(f"Time for reconstruction: {(stop - start) * 1000} ms")
-    output = output.view(OUT_SIZE, OUT_SIZE).detach().numpy()
-    if original_image is not None:
-        plt.subplot(1, 2, 1)
-        plt.imshow(output)
-        plt.title(title)
-        plt.subplot(1, 2, 2)
-        plt.imshow(original_image)
-        plt.title("Original")
-        # add colorbar
-        plt.colorbar()
-        plt.show()
-    else:
-        plt.imshow(output)
-        plt.title(title)
-        # add colorbar
-        plt.colorbar()
-        plt.show()
-
-
-def plot_loss(val_loss_list, loss_list=None, save_name=""):
-    """
-    Plots the loss and validation loss
-    :param loss_list:
-    :param val_loss_list:
-    :return:
-    """
-    if loss_list is not None:
-        plt.plot(loss_list)
-    plt.plot(val_loss_list)
-    if loss_list is not None:
-        plt.legend(["Train", "Validation"])
-    else:
-        plt.legend(["Validation"])
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    if save_name != "":
-        plt.savefig(save_name)
-    plt.show()
-
+        counter += 1
+        print(f"Early stopping in {patience - counter} epochs")
+        if counter >= patience:
+            print("Early stopping triggered. No improvement in validation loss.")
+            # save the model
+            torch.save(best_model.state_dict(),
+                       os.path.join(model_path,
+                                    f"model_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_epoche_{epoch}_of_{num_epochs}_best_model.pth"))
+            return True
+    return False
 
 
 if __name__ == "__main__":
@@ -180,15 +61,15 @@ if __name__ == "__main__":
     SAVE_CHECKPOINTS = False
     LOSS_PLOT_INTERVAL = 10
     # Training parameters
-    num_epochs = 150
-    NOISE_LEVEL = 0.05
+    num_epochs = 200
+    NOISE_LEVEL = 0.0
     # NOISE_LEVEL = 0
     LEARNING_RATE = 0.0005
     # Define the weight decay factor
     weight_decay = 1e-5  # Adjust this value as needed (L2 regularization)
     # weight_decay = 0  # Adjust this value as needed (L2 regularization)
     # Define early stopping parameters
-    patience = 30  # Number of epochs to wait for improvement
+    patience = 10  # Number of epochs to wait for improvement
 
     best_val_loss = float('inf')  # Initialize with a very high value
     counter = 0  # Counter to track epochs without improvement
@@ -217,6 +98,11 @@ if __name__ == "__main__":
     v0 = np.load("Own_Simulation_Dataset/v0.npy")
     # subtract v0 from all voltages
     voltage_data_np = voltage_data_np - v0
+
+    # reduce the number of images to 1000
+    image_data_np = image_data_np[:4000]
+    voltage_data_np = voltage_data_np[:4000]
+
     # Now the model should learn the difference between the voltages and v0 (default state)
 
     print("Overall data shape: ", voltage_data_np.shape)
@@ -281,6 +167,7 @@ if __name__ == "__main__":
                 os.path.join(model_path, "MODEL_NAME.pth")))
         loss_list = []
         val_loss_list = []
+        best_model = model
         for epoch in range(num_epochs):
             model.train()  # Set the model to training mode
             for batch_voltages, batch_images in train_dataloader:
@@ -304,20 +191,10 @@ if __name__ == "__main__":
                     val_loss += criterion(outputs, batch_images.view(-1, OUT_SIZE ** 2)).item() * LOSS_SCALE_FACTOR
 
                 val_loss /= len(val_dataloader)
-                if val_loss < best_val_loss:    # Early stopping
-                    best_val_loss = val_loss
-                    counter = 0
-                    best_model = copy.deepcopy(model)
-                else:
-                    counter += 1
-                    print(f"Early stopping in {patience-counter} epochs")
-                    if counter >= patience:
-                        print("Early stopping triggered. No improvement in validation loss.")
-                        # save the model
-                        torch.save(best_model.state_dict(),
-                                      os.path.join(model_path,
-                                                    f"model_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_epoche_{epoch}_of_{num_epochs}_best_model.pth"))
-                        break
+                out = handle_early_stopping()  # Early stopping
+                if out:
+                    break
+
                 val_loss_list.append(val_loss)
                 print(f"Epoch [{epoch + 1}/{num_epochs}], Val Loss: {round(val_loss, 4)} Training Loss: {round(loss.item(), 4)}")
 
@@ -351,14 +228,6 @@ if __name__ == "__main__":
             "Own_Simulation_Dataset/Models/model2023-08-02_18-32-46/model_2023-08-02_18-33-07_10_epochs.pth"))
         model.eval()
 
-    # After training, you can use the model to reconstruct images
-    # by passing voltage data to the model's forward method.
-
-    # Try inference on train images
-    # Try inference on train images
-    # print("Train images")
-    # plot_sample_reconstructions(train_images, train_voltage, model, criterion, num_images=20)
-
     # Try inference on test images
     print("Test_max_noise_05 images")
     plot_sample_reconstructions(test_images, test_voltage, model, criterion, num_images=20,
@@ -368,8 +237,3 @@ if __name__ == "__main__":
     # voltage_data_tensor = torch.tensor(single_datapoint, dtype=torch.float32)
     # plot_single_reconstruction(model=model, voltage_data=voltage_data_tensor)
 
-    # Try inference on test images
-    # print("2023_08_02_15_30 images")
-    # plot_reconstruction(test_images, test_voltage, model, criterion, num_images=20,
-    #                     save_path="Edinburgh mfEIT Dataset/models_new_loss_methode/2"
-    #                     )
---
+++ model_plot_utils.py
@@ -0,0 +1,153 @@
+import os
+import time
+
+import cv2
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+
+# from eidnburgh_cnn_test import OUT_SIZE, LOSS_SCALE_FACTOR
+
+LOSS_SCALE_FACTOR = 1000
+OUT_SIZE = 64
+
+def calc_average_loss_completly_black(image_data_tensor, criterion):
+    """
+    To check if the network is learning anything at all, we can check the average loss of the network when the input is
+    completly black. If the network is learning anything, the average loss should be high.
+    (Check if criterion is suitable for this)
+    :param image_data_tensor:
+    :param voltage_data_tensor:
+    :param criterion:
+    :return:
+    """
+    # Display all reconstructed images
+    average_loss = 0
+    for img in image_data_tensor:
+        output = np.zeros((OUT_SIZE, OUT_SIZE))
+        loss = round(criterion(torch.tensor(output), img.view(OUT_SIZE, OUT_SIZE)).item(), 4) * LOSS_SCALE_FACTOR
+        average_loss += loss
+    average_loss /= len(image_data_tensor)  # easier to read
+    print(f"Average loss black: {average_loss}")
+    return average_loss
+
+
+def calc_average_loss_completly_white(image_data_tensor, criterion):
+    # Display all reconstructed images
+    average_loss = 0
+    for img in image_data_tensor:
+        output = np.ones((OUT_SIZE, OUT_SIZE))
+        loss = round(criterion(torch.tensor(output), img.view(OUT_SIZE, OUT_SIZE)).item(), 4) * LOSS_SCALE_FACTOR
+        average_loss += loss
+    average_loss /= len(image_data_tensor)  # easier to read
+    print(f"Average loss white: {average_loss}")
+    return average_loss
+
+
+def plot_sample_reconstructions(image_data_tensor, voltage_data_tensor, model, criterion, num_images=40,
+                                save_path=""):
+    """
+    Creates a few random reconstructions and plots them
+    :param image_data_tensor:
+    :param voltage_data_tensor:
+    :param model:
+    :param criterion:
+    :param num_images:
+    :param save_path:
+    :return:
+    """
+    global output
+    # Display all reconstructed images
+    average_loss = 0
+    # select random images
+    random_indices = np.random.randint(0, len(image_data_tensor), num_images)
+    for i in random_indices:
+        img = image_data_tensor[i]
+        img_numpy = img.view(OUT_SIZE, OUT_SIZE).detach().numpy()
+        volt = voltage_data_tensor[i]
+        output = model(volt)
+        output = output.view(OUT_SIZE, OUT_SIZE).detach().numpy()
+        cv2.imshow("Reconstructed Image", cv2.resize(output, (256, 256)))
+        cv2.imshow("Original Image", cv2.resize(img_numpy, (256, 256)))
+        # print loss for each image
+        a = torch.tensor(output)
+        b = img.view(OUT_SIZE, OUT_SIZE)
+        loss = round(criterion(a, b).item(), 4)
+        loss = loss * LOSS_SCALE_FACTOR  # easier to read
+        average_loss += loss
+        cv2.waitKey(1)
+        print(f"Loss: {loss}")
+        # plot comparison with matplotlib
+        plt.subplot(1, 2, 1)
+        plt.imshow(output)
+        plt.title(f"Loss: {loss}")
+        plt.subplot(1, 2, 2)
+        plt.imshow(img_numpy)
+        plt.title("Original")
+        # add colorbar
+        plt.colorbar()
+        if save_path != "":
+            plt.savefig(os.path.join(save_path, f"reconstruction_{i}.png"))
+        plt.show()
+        time.sleep(0.1)
+    return average_loss / num_images
+
+
+def plot_single_reconstruction(model, voltage_data, title = "Reconstructed image", original_image:np.array=None):
+    """
+    Plots a single reconstruction using the model
+    :param model:
+    :param voltage_data:
+    :return:
+    """
+    if type(voltage_data) == np.ndarray:
+        voltage_data = torch.tensor(voltage_data, dtype=torch.float32)
+    elif type(voltage_data) == torch.Tensor:
+        # if dtype is not float32, convert it
+        if voltage_data.dtype != torch.float32:
+            voltage_data = voltage_data.type(torch.float32)
+    else:
+        raise TypeError(
+            f"voltage_data_tensor must be either a numpy array or a torch tensor but is {type(voltage_data)}")
+    start = time.time()
+    output = model(voltage_data)
+    stop = time.time()
+    print(f"Time for reconstruction: {(stop - start) * 1000} ms")
+    output = output.view(OUT_SIZE, OUT_SIZE).detach().numpy()
+    if original_image is not None:
+        plt.subplot(1, 2, 1)
+        plt.imshow(output)
+        plt.title(title)
+        plt.subplot(1, 2, 2)
+        plt.imshow(original_image)
+        plt.title("Original")
+        # add colorbar
+        plt.colorbar()
+        plt.show()
+    else:
+        plt.imshow(output)
+        plt.title(title)
+        # add colorbar
+        plt.colorbar()
+        plt.show()
+
+
+def plot_loss(val_loss_list, loss_list=None, save_name=""):
+    """
+    Plots the loss and validation loss
+    :param loss_list:
+    :param val_loss_list:
+    :return:
+    """
+    if loss_list is not None:
+        plt.plot(loss_list)
+    plt.plot(val_loss_list)
+    if loss_list is not None:
+        plt.legend(["Train", "Validation"])
+    else:
+        plt.legend(["Validation"])
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    if save_name != "":
+        plt.savefig(save_name)
+    plt.show()
