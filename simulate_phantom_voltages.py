e7a658c3ef124dee54b095005fe8e8e5f893228c
Lars_Labor_PC <larsgudjons@gmail.com>
2023-08-09 12:58:27 +0200

    Added methode to look at data in dataset.

--- simulate_phantom_voltages.py
+++ simulate_phantom_voltages.py
@@ -143,69 +143,74 @@ def solve_eit_using_jac(mesh_new, mesh_obj, protocol_obj, v0, v1):
 #     cv2.imshow('average', cv2.resize(average_image, (256, 256)))
 #     cv2.waitKey(0)
 
+
+def look_at_dataset(img_array, v1_array, v0):
+    print(img_array.shape)
+    print(v1_array.shape)
+    average_image = np.mean(img_array, axis=0)*10
+    # clip between 0 and 255
+    average_image = np.clip(average_image, 0, 255)
+
+    cv2.imshow('average', cv2.resize(average_image, (256, 256)))
+    for i, img in enumerate(img_array):
+        voltage_differece = v1_array[i] - v0
+        # show voltage difference and image in one plot
+        plt.subplot(1, 2, 1)
+        plt.imshow(img*10)
+        plt.subplot(1, 2, 2)
+        plt.plot(voltage_differece)
+        plt.show()
+        cv2.imshow('img', cv2.resize(img*10, (256, 256)))
+
+        cv2.waitKey(100)
+    # cv2.waitKey(0)
+
 if __name__ == '__main__':
-    """ 0. build mesh """
-    DATA_COLLECTION_RUN = 0
-    SAMPLES = 5000
-    mesh_obj = mesh.create(n_el, h0=0.1)
-    # The mesh has 704 elements
-    # extract node, element, alpha
-    pts = mesh_obj.node
-    # pts is the list of the nodes of the mesh (with coordinates)
-    tri = mesh_obj.element
-    # tri is the list of the elements of the mesh (with the nodes that compose them)
-    x, y = pts[:, 0], pts[:, 1]
-
-    img_array = []
-    v0_array = []
-    v1_array = []
-    # Simulate 1 sample to get the v0
-    v0, v1, img = generate_sample_mesh_simulation(mesh_obj=mesh_obj, n_el=32)
-    np.save("Own_Simulation_Dataset/v0.npy", v0)
-
-    # Simulate the rest of the samples
-    for i in range(10):
-        start = time.time()
-        DATA_COLLECTION_RUN += 1
-        for i in range(SAMPLES):
-            print(i)
-            v0, v1, img = generate_sample_mesh_simulation(mesh_obj=mesh_obj, n_el=32)
-            img_array.append(img)
-            v0_array.append(v0)
-            v1_array.append(v1)
-        print()
-        end = time.time()
-        print(f"Time elapsed for {SAMPLES} samples: {end - start}")
-        print("Average time per sample: ", (end - start) / SAMPLES)
-        img_array_np = np.array(img_array)
-        np.save(f"Own_Simulation_Dataset/img_array_{DATA_COLLECTION_RUN}.npy", img_array_np)
-        v1_array_np = np.array(v1_array)
-        np.save(f"Own_Simulation_Dataset/v1_array_{DATA_COLLECTION_RUN}.npy", v1_array_np)
-        print("OK")
-
-
-    # combine_multiple_data_files()
+    # """ 0. build mesh """
+    # DATA_COLLECTION_RUN = 0
+    # SAMPLES = 5000
+    # mesh_obj = mesh.create(n_el, h0=0.1)
+    # # The mesh has 704 elements
+    # # extract node, element, alpha
+    # pts = mesh_obj.node
+    # # pts is the list of the nodes of the mesh (with coordinates)
+    # tri = mesh_obj.element
+    # # tri is the list of the elements of the mesh (with the nodes that compose them)
+    # x, y = pts[:, 0], pts[:, 1]
     #
-    # img_array = np.load("Own_Simulation_Dataset/img_array.npy")
-    # v1_array = np.load("Own_Simulation_Dataset/v1_array.npy")
-    # print(img_array.shape)
-    # print(v1_array.shape)
-    # average_image = np.mean(img_array, axis=0)*10
-    # # clip between 0 and 255
-    # average_image = np.clip(average_image, 0, 255)
+    # img_array = []
+    # v0_array = []
+    # v1_array = []
+    # # Simulate 1 sample to get the v0
+    # v0, v1, img = generate_sample_mesh_simulation(mesh_obj=mesh_obj, n_el=32)
+    # np.save("Own_Simulation_Dataset/v0.npy", v0)
     #
-    # cv2.imshow('average', cv2.resize(average_image, (256, 256)))
-    # for img in img_array:
-    #     cv2.imshow('average', cv2.resize(img*10, (256, 256)))
-    #     cv2.waitKey(100)
-    # # cv2.waitKey(0)
+    # # Simulate the rest of the samples
+    # for i in range(10):
+    #     start = time.time()
+    #     DATA_COLLECTION_RUN += 1
+    #     for i in range(SAMPLES):
+    #         print(i)
+    #         v0, v1, img = generate_sample_mesh_simulation(mesh_obj=mesh_obj, n_el=32)
+    #         img_array.append(img)
+    #         v0_array.append(v0)
+    #         v1_array.append(v1)
+    #     print()
+    #     end = time.time()
+    #     print(f"Time elapsed for {SAMPLES} samples: {end - start}")
+    #     print("Average time per sample: ", (end - start) / SAMPLES)
+    #     img_array_np = np.array(img_array)
+    #     np.save(f"Own_Simulation_Dataset/img_array_{DATA_COLLECTION_RUN}.npy", img_array_np)
+    #     v1_array_np = np.array(v1_array)
+    #     np.save(f"Own_Simulation_Dataset/v1_array_{DATA_COLLECTION_RUN}.npy", v1_array_np)
+    #     print("OK")
+
+    img_array = np.load("Own_Simulation_Dataset/img_array.npy")
+    v1_array = np.load("Own_Simulation_Dataset/v1_array.npy")
+    v0 = np.load("Own_Simulation_Dataset/v0.npy")
+    look_at_dataset(img_array, v1_array, v0)
 
 
-
-
-    # # time generate_random_anomaly_parameters with timeit
-    # import timeit
-    # print(timeit.timeit("generate_random_anomaly_parameters(0.1, 0.2, 1, 1)", setup="from __main__ import generate_random_anomaly_parameters", number=100000))
 
 
 
