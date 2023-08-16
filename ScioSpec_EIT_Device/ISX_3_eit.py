cf11dd555afb64a112bb7ca8c75dfca354589aac
Lars_Labor_PC <larsgudjons@gmail.com>
2023-08-16 14:32:55 +0200

    Implemented more of the ISX3 COM interface

--- ScioSpec_EIT_Device/ISX_3_eit.py
+++ ScioSpec_EIT_Device/ISX_3_eit.py
@@ -150,73 +150,175 @@ def set_fe_settings(handle, measurment_mode, measurment_channel, range_setting):
     read_ack(handle)
 
 
-def start_eit(handle):
+def set_eit_setup_burst(handle, burst_count):
     """
-    Starts the EIT measurement
+    Sets the EIT setup burst count (Number of frames generated before measurement stops automatically.)
+    For continuous streaming set to 0
+    :param handle:
+    :param burst_count:Number of frames generated before measurement stops automatically [0-65535]
+    :return:
+    """
+    # convert burst count to 2 bytes
+    burst_count = burst_count.to_bytes(2, byteorder='big')
+    burst_count_array = bytearray(burst_count)
+    cmd = bytearray([0xC4, 0x03, 0x02]) + burst_count_array + bytearray([0xC4])
+    write_data_to_device(handle, cmd)
+    read_ack(handle)
+
+def get_eit_setup_burst(handle):
+    """
+    Gets the EIT setup burst count (Number of frames generated before measurement stops automatically.)
     :param handle:
     :return:
     """
-    cmd = bytearray([0xB8, 0x00, 0xB8])
+    cmd = bytearray([0xC5, 0x01, 0x02, 0xC5])
+    write_data_to_device(handle, cmd)
+    read_buffer = read_data_until_command_ends(handle)
+    print(f"Read buffer: {read_buffer}")
+    if read_ack(handle):
+        burst_count = int.from_bytes(read_buffer[3:5], byteorder='big')
+       #  return result as dictionary
+        return {"burst_count": burst_count}
+    else:
+        return None
+
+def set_eit_setup_precision(handle, precision):
+    """
+    Sets the EIT setup precision (Precision of the measurement in ms)
+    :param handle:
+    :param precision: Precision of the measurement [1-10]
+    :return:
+    """
+    cmd = bytearray([0xC4, 0x02, 0x03, precision, 0xC4])
+    write_data_to_device(handle, cmd)
+    read_ack(handle)
+
+def get_eit_setup_precision(handle):
+    """
+    Gets the EIT setup precision (Precision of the measurement in ms)
+    :param handle:
+    :return:
+    """
+    cmd = bytearray([0xC5, 0x01, 0x03, 0xC5])
+    write_data_to_device(handle, cmd)
+    read_buffer = read_data_until_command_ends(handle)
+    print(f"Read buffer: {read_buffer}")
+    if read_ack(handle):
+        precision = read_buffer[3]
+        # return result as dictionary
+        return {"precision": precision}
+    else:
+        return None
+
+def set_eit_setup_frequency(handle, f_min, f_max, f_count, f_type):
+    """
+    Sets the EIT setup frequency range.
+    Add excitation frequency block in Hz. To build a custom excitation frequency stack the command can be send
+    multiple times. The frequencies are placed in the stack as they are configured. The total number of configured
+    excitation frequencies must not exceed the excitation frequency stack length of 128.
+    :param handle:
+    :param f_min: Minimum frequency in Hz [0.1-10M] # 4 bytes
+    :param f_max: Maximum frequency in Hz [0.1-10M] # 4 bytes
+    :param f_count: Number of frequencies [1-128] # 2 byte unsigned
+    :param f_type: Type of frequencies [0-1] # 1 byte   0: Linear, 1: Logarithmic
+    :return:
+    """
+    # convert fmin and fmax to 4 byte array
+    bytes_f_min_array = convert_float_to_bytes(f_min)
+
+    bytes_f_max_array = convert_float_to_bytes(f_max)
+
+    # bytes_f_min_array = bytearray.fromhex('44 7a 00 00')
+    # bytes_f_max_array = bytearray.fromhex('44 fa 00 00')
+    # convert f count to 2 byte array
+    bytes_f_count = f_count.to_bytes(2, byteorder='big')
+    bytes_f_count_array = bytearray(bytes_f_count)
+    cmd = bytearray([0xC4, 0x0C, 0x04]) + bytes_f_min_array + bytes_f_max_array + bytes_f_count_array + bytearray([f_type, 0xC4])
     write_data_to_device(handle, cmd)
     read_ack(handle)
 
 
-def set_eit_setup(handle, burst_count, precision):
+def convert_float_to_bytes(float_value):
     """
-    Sets the EIT setup
-    :param handle:
-    :param burst_count:
-    :param precision:
+    Converts a float value to a 4 byte array by the IEEE 754 standard.
+    :param float_value:
     :return:
     """
+    b = struct.pack('f', float_value)
+    bytes_array = bytearray(b)
+    # change endianness
+    bytes_array.reverse()
+    return bytes_array
+
+
+def get_eit_setup_frequency(handle):
+    """
+    Gets the EIT setup frequency range.
+    :param handle:
+    :return:
+    """
+    cmd = bytearray([0xC5, 0x01, 0x04, 0xC5])
+    write_data_to_device(handle, cmd)
+    read_buffer = read_data_until_command_ends(handle)
+    print(f"Read buffer: {read_buffer}")
+    if read_ack(handle):
+        f_min = read_buffer[3]
+        f_max = read_buffer[4]
+        f_count = read_buffer[5]
+        f_type = read_buffer[6]
+        # return result as dictionary
+        return {"f_min": f_min, "f_max": f_max, "f_count": f_count, "f_type": f_type}
+    else:
+        return None
+
+def set_eit_amplitude(handle, amplitude):
+    """
+    Sets the EIT setup amplitude.
+    :param handle:
+    :param amplitude: could be either 32Bit float ([LE] = 05) or 64Bit float ([LE] = 09) (double)
+                        excitation amplitude in Ampere
+                        range 0.00001A - 0.01A
+    :return:
+    """
+    if amplitude < 0.00001 or amplitude > 0.01:
+        raise ValueError("Amplitude must be between 0.00001 and 0.01")
+
+    amplitude_bytes = convert_float_to_bytes(amplitude)
+    cmd = bytearray([0xC4, 0x05, 0x05]) + amplitude_bytes + bytearray([0xC4])
+    write_data_to_device(handle, cmd)
+    read_ack(handle)
+
+def get_eit_amplitude(handle):
+    """
+    Gets the EIT setup amplitude.
+    :param handle:
+    :return:
+    """
+    cmd = bytearray([0xC5, 0x01, 0x05, 0xC5])
+    write_data_to_device(handle, cmd)
+    read_buffer = read_data_until_command_ends(handle)
+    print(f"Read buffer: {read_buffer}")
+    if read_ack(handle):
+        amplitude = read_buffer[2:6]
+        print(f"Amplitude: {amplitude}")
+        # return result as dictionary
+        return {"amplitude": amplitude}
+    else:
+        return None
+
+def reset_eit_setup(handle):
     cmd = bytearray([0xC4, 0x01, 0x01, 0xC4])
     write_data_to_device(handle, cmd)
     read_ack(handle)
-    cmd = bytearray([0xC4, 0x03, 0x02]) + struct.pack(">H", burst_count) + bytearray([0xC4])
-    write_data_to_device(handle, cmd)
-    read_ack(handle)
-    cmd = bytearray([0xC4, 0x05, 0x03]) + struct.pack(">H", precision) + bytearray([0xC4])
-    write_data_to_device(handle, cmd)
-    read_ack(handle)
 
 
-# def get_eit_setup(handle):
-#     cmd = bytearray([0xC5, 0x01, 0x02, 0xC5])
-#     write_data_to_device(handle, cmd)
-#     read_buffer = read_data(handle, 4)
-#     burst_count = struct.unpack(">H", read_buffer[2:4])[0]
-#     print(f"Read buffer: {read_buffer}")
-#     print(f"Burst count: {burst_count}")
-#
-#     cmd = bytearray([0xC5, 0x01, 0x03, 0xC5])
-#     write_data_to_device(handle, cmd)
-#     read_buffer = read_data(handle, 4)
-#     print(f"Read buffer: {read_buffer}")
-#     precision = struct.unpack(">H", read_buffer[2:6])[0]
-#
-#     return burst_count, precision
 
-def set_excitation_frequencies(handle, fmin, fmax, fcount, ftype):
-    """
-    Sets the excitation frequencies
-    :param handle:
-    :param fmin:
-    :param fmax:
-    :param fcount:
-    :param ftype:
-    :return:
-    """
-    cmd = bytearray([0xC4, 0x0C, 0x04])
-    cmd += struct.pack(">f", fmin)
-    cmd += struct.pack(">f", fmax)
-    cmd += struct.pack(">H", fcount)
-    cmd += struct.pack("B", ftype)
-    cmd += bytearray([0xC4])
+def start_measurement(handle, on_off):
+
+    cmd = bytearray([0xB8, 0x01, on_off, 0xB8])
     write_data_to_device(handle, cmd)
     read_ack(handle)
 
-
-# def set_extraction_amplitude(handle):
 
 def main():
     port = "COM3"  # Replace with the correct serial port name
@@ -228,15 +330,31 @@ def main():
     print("Connection established")
     handle.flush()
 
-    # reset_setup(handle)
+    reset_setup(handle)
     #
     # save_settings(handle)
     # set_options(handle, 0x01, 0x00)
     # print(get_options(handle, option_id=0x01))
 
-    set_fe_settings(handle, 0x01, 0x03, 0x01)
+    reset_eit_setup(handle)
 
-    print(get_fe_settings(handle))
+    set_eit_setup_burst(handle, 0)
+    print(get_eit_setup_burst(handle))
+    #
+    set_eit_setup_precision(handle, 0x01)
+    print(get_eit_setup_precision(handle))
+
+    set_eit_setup_frequency(handle, 1, 2, 1, 0)
+    # print(get_eit_setup_frequency(handle))
+
+    set_eit_amplitude(handle, 0.01)
+    print(get_eit_amplitude(handle))
+
+    # set_fe_settings(handle, 0x01, 0x03, 0x01)
+    #
+    # print(get_fe_settings(handle))
+    #
+    start_measurement(handle, 0x01)
 
     # reset_system(handle)
 
