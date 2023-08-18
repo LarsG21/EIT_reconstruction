816dc80c21985180d56d8a770375e72e08c1e257
Lars_Labor_PC <larsgudjons@gmail.com>
2023-08-18 17:08:11 +0200

    introduced plotting methods that can be used for general eit setups for visualization of FEM.

--- fem_forward_plot.py
+++ fem_forward_plot.py
@@ -9,6 +9,9 @@ import pyeit.mesh as mesh
 from plot_utils import plot_results_fem_forward
 from pyeit.mesh.shape import thorax
 from pyeit.mesh.wrapper import PyEITAnomaly_Circle
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 """ 0. build mesh """
 n_el = 16  # nb of electrodes
@@ -22,12 +25,80 @@ else:
 mesh_obj.print_stats()
 
 # change permittivity
-anomaly = PyEITAnomaly_Circle(center=[0.4, 0.5], r=0.2, perm=100.0)
+# anomaly = PyEITAnomaly_Circle(center=[0.4, 0.5], r=0.3, perm=100.0)
+anomaly = []
 mesh_new = mesh.set_perm(mesh_obj, anomaly=anomaly, background=1.0)
 
 """ 1. FEM forward simulations """
 # setup EIT scan conditions
 protocol_obj = protocol.create(n_el, dist_exc=1, step_meas=1, parser_meas="std")
 
-for line in protocol_obj.ex_mat:
-    plot_results_fem_forward(mesh=mesh_new, line=line)
+
+def plot_potentail_and_e_field_for_all_injections(mesh, protocol_obj):
+    """
+    Plot the equi-potential lines and the E-field lines for all injections in the protocol object.
+    :param mesh:
+    :param protocol_obj:
+    :return:
+    """
+    for line in protocol_obj.ex_mat:
+        # plot_results_fem_forward(mesh=mesh_new, line=line)
+
+        equi_potential_image, e_field_image = plot_results_fem_forward(mesh=mesh_new, line=line)
+
+        plt.figure(figsize=(12, 6))
+
+        # Display equi-potential image
+        plt.subplot(1, 2, 1)
+        plt.imshow(equi_potential_image)
+        plt.title("Equi-potential Image")
+        plt.axis('off')
+
+        # Display E-field image
+        plt.subplot(1, 2, 2)
+        plt.imshow(e_field_image)
+        plt.title("E-Field Image")
+        plt.axis('off')
+
+        plt.tight_layout()
+        plt.show()
+
+
+def plot_potential_lines_for_all_injections(mesh, protocol_obj):
+    """
+    Plot the equi-potential lines for all injections in the protocol object.
+    :param mesh:
+    :param protocol_obj:
+    :return:
+    """
+    equi_potential_images = []
+
+    for line in protocol_obj.ex_mat:
+        equi_potential_image, _ = plot_results_fem_forward(mesh=mesh, line=line)
+        equi_potential_images.append(equi_potential_image)
+    # take only every 4th image
+    equi_potential_images = equi_potential_images[::4]
+    # Calculate the number of rows and columns for the subplot grid
+    num_images = len(equi_potential_images)
+    num_cols = 4  # Number of columns in the subplot grid
+    num_rows = (num_images + num_cols - 1) // num_cols  # Ceiling division
+
+    # Create a new figure and subplot grid
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 5 * num_rows))
+
+    # Iterate through the images and axes to plot them
+    for i, (equi_potential_image, ax) in enumerate(zip(equi_potential_images, axes.flatten())):
+        ax.imshow(equi_potential_image)
+        # ax.set_title(f"Equi-potential Image {i+1}")
+        ax.axis('off')
+
+    # Adjust layout and display the plot
+    plt.tight_layout()
+    # save the plot
+    plt.savefig("equi-potential-lines.png")
+    plt.show()
+
+
+if __name__ == '__main__':
+    # plot_potentail_and_e_field_for_all_injections(mesh=mesh_new, protocol_obj=protocol_obj)
+    plot_potential_lines_for_all_injections(mesh=mesh_new, protocol_obj=protocol_obj)
--- plot_utils.py
+++ plot_utils.py
@@ -1,10 +1,16 @@
 from __future__ import division, absolute_import, print_function
 
 import numpy as np
+from PIL.Image import Image
 from matplotlib import pyplot as plt
 
 from pyeit.eit.fem import Forward
 from pyeit.eit.interp2d import pdegrad, sim2pts
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
 def plot_results_fem_forward(mesh, line):
@@ -12,7 +18,7 @@ def plot_results_fem_forward(mesh, line):
     Plot results of FEM forward simulation. Plots the equi-potential lines and the E-Field lines.
     :param mesh: the mesh object used for the FEM forward simulation
     :param line: Input and output electrodes as a 2x1 nparray
-    :return:
+    :return: Tuple of PIL Image objects containing the equi-potential and E-field plots
     """
     print(f"plot_results_fem_forward between {line[0]} and {line[1]}")
     # extract node, element, alpha
@@ -27,16 +33,17 @@ def plot_results_fem_forward(mesh, line):
     fwd = Forward(mesh)
     f = fwd.solve(ex_line)
     f = np.real(f)
-    """ 2. plot """
-    fig, ax1 = plt.subplots(figsize=(9, 6))
+
+    """ 2. plot equi-potential lines """
+    fig_equipotential, ax1 = plt.subplots(figsize=(9, 6))
     # draw equi-potential lines
     vf = np.linspace(min(f), max(f), 64)
     # vf = np.sort(f[el_pos])
     # Draw contour lines on an unstructured triangular grid.
-    ax1.tricontour(x, y, tri, f, vf, cmap=plt.cm.viridis)
+    contour = ax1.tricontour(x, y, tri, f, vf, cmap=plt.cm.viridis)
     # draw mesh structure
     # Create a pseudocolor plot of an unstructured triangular grid
-    ax1.tripcolor(
+    mesh_plot = ax1.tripcolor(
         x,
         y,
         tri,
@@ -50,24 +57,40 @@ def plot_results_fem_forward(mesh, line):
     ax1.plot(x[el_pos], y[el_pos], "ro")
     for i, e in enumerate(el_pos):
         ax1.text(x[e], y[e], str(i + 1), size=12)
-    ax1.set_title("equi-potential lines")
+    # ax1.set_title("equi-potential lines")
     # clean up
     ax1.set_aspect("equal")
     ax1.set_ylim([-1.2, 1.2])
     ax1.set_xlim([-1.2, 1.2])
-    fig.set_size_inches(6, 6)
-    # fig.savefig('demo_bp.png', dpi=96)
-    plt.show()
+    fig_equipotential.set_size_inches(6, 6)
+
+    # Render the plot to an image
+    # Draw the content
+    fig_equipotential.canvas.draw()
+    width, height = fig_equipotential.canvas.get_width_height()
+    image_array = np.frombuffer(fig_equipotential.canvas.tostring_rgb(), dtype='uint8')
+    equi_potential_image = image_array.reshape(height, width, 3)
+
+    """ 3. plot E field (logmag) """
     ux, uy = pdegrad(pts, tri, f)
     uf = ux ** 2 + uy ** 2
     uf_pts = sim2pts(pts, tri, uf)
     uf_logpwr = 10 * np.log10(uf_pts)
-    fig, ax = plt.subplots(figsize=(9, 6))
+    fig_e_field, ax = plt.subplots(figsize=(9, 6))
     # Draw contour lines on an unstructured triangular grid.
-    ax.tripcolor(x, y, tri, uf_logpwr, cmap=plt.cm.viridis)
+    field_plot = ax.tripcolor(x, y, tri, uf_logpwr, cmap=plt.cm.viridis)
     ax.tricontour(x, y, tri, uf_logpwr, 10, cmap=plt.cm.hot)
     ax.set_aspect("equal")
     ax.set_ylim([-1.2, 1.2])
     ax.set_xlim([-1.2, 1.2])
     ax.set_title("E field (logmag)")
-    plt.show()
+
+    # Render the plot to an image
+    fig_e_field.canvas.draw()
+    width, height = fig_e_field.canvas.get_width_height()
+    image_array = np.frombuffer(fig_e_field.canvas.tostring_rgb(), dtype='uint8')
+    e_field_image = image_array.reshape(height, width, 3)
+    # clear the figure
+    plt.close(fig_equipotential)
+    plt.close(fig_e_field)
+    return equi_potential_image, e_field_image
