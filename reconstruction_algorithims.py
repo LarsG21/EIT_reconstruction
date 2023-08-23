a7bf7428b1df813b0c54d1aecfefc72da892b39c
Lars_Labor_PC <larsgudjons@gmail.com>
2023-08-23 14:49:24 +0200

    Moved reconstruction methods to separate files

---
+++ reconstruction_algorithims.py
@@ -0,0 +1,130 @@
+import time
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from pyeit.eit import greit, jac, bp
+from pyeit.eit.interp2d import sim2pts
+
+
+def solve_and_plot_greit(v0, v1, mesh_obj, protocol_obj, path1_for_name_only, path2_for_name_only):
+    """
+    Solve the EIT problem with GREIT and plot the result
+    :param v0: default voltages (no anomaly)
+    :param v1: voltages with anomaly
+    :param mesh_obj: mesh object
+    :param protocol_obj: protocol object
+    :param path1_for_name_only:
+    :param path2_for_name_only:
+    :return:
+    """
+
+    # extract node, element, alpha
+    pts = mesh_obj.node
+    # pts is the list of the nodes of the mesh (with coordinates)
+    tri = mesh_obj.element
+    # tri is the list of the elements of the mesh (with the nodes that compose them)
+    x, y = pts[:, 0], pts[:, 1]
+    eit = greit.GREIT(mesh_obj, protocol_obj)
+    eit.setup(p=0.50, lamb=0.01, perm=1, jac_normalized=True)
+    ds = eit.solve(v1, v0, normalize=True)
+    x, y, ds = eit.mask_value(ds, mask_value=np.NAN)
+
+    # show alpha
+    fig, axes = plt.subplots(1, 1, constrained_layout=True)
+    name1 = path1_for_name_only.split("\\")[-1]
+    name2 = path2_for_name_only.split("\\")[-1]
+    plt.title(f"{name1} - {name2}")
+
+    im = axes.imshow(np.real(ds), interpolation="none", cmap=plt.cm.viridis)
+    axes.axis("equal")
+
+    fig.colorbar(im, ax=axes)
+    # fig.savefig('../doc/images/demo_greit.png', dpi=96)
+    plt.show()
+
+
+def solve_and_plot_jack(v0, v1, mesh_obj, protocol_obj, save_path=None, path1_for_name_only=None,
+                        path2_for_name_only=None):
+    """
+    Reconstruct using JAC
+    :param v0:
+    :param v1:
+    :param mesh_obj:
+    :param protocol_obj:
+    :param save_path:
+    :param path1_for_name_only:
+    :param path2_for_name_only:
+    :return:
+    """
+    pts = mesh_obj.node
+    tri = mesh_obj.element
+    x, y = pts[:, 0], pts[:, 1]
+    eit = jac.JAC(mesh_obj, protocol_obj)
+    eit.setup(p=0.5, lamb=0.01, method="kotre", perm=1, jac_normalized=True)
+    start_time = time.time()
+    ds = eit.solve(v1, v0, normalize=True)
+    # print(f"JAC took {(time.time() - start_time)*1000} ms")
+    ds_n = sim2pts(pts, tri, np.real(ds))
+    # ds_n is the delta sigma interpolated on the mesh nodes
+    # plot ground truth
+    fig, axes = plt.subplots(1, 1, constrained_layout=True)
+    if path1_for_name_only is None or path2_for_name_only is None:
+        plt.title("JAC solver")
+    else:
+        name1 = path1_for_name_only.split("\\")[-1]
+        name2 = path2_for_name_only.split("\\")[-1]
+        plt.title(f"{name1} - {name2}")
+    # plot EIT reconstruction
+    im = axes.tripcolor(x, y, tri, ds_n, shading="flat")
+    for i, e in enumerate(mesh_obj.el_pos):
+        axes.annotate(str(i + 1), xy=(x[e], y[e]), color="r")
+    axes.set_aspect("equal")
+    fig.colorbar(im, ax=axes)
+    if save_path:
+        fig.savefig(save_path, dpi=96)
+    plt.show()
+    # plt.draw()    # For non Pycharm Plotting
+    # plt.pause(0.1)
+    # # close all figures to display the next one
+    # print("done")
+
+
+def solve_and_plot_bp(v0, v1, mesh_obj, protocol_obj, path1_for_name_only=None, path2_for_name_only=None):
+    """
+    Reconstruct using BP
+    :param v0:
+    :param v1:
+    :param mesh_obj:
+    :param protocol_obj:
+    :param path1_for_name_only:
+    :param path2_for_name_only:
+    :return:
+    """
+    eit = bp.BP(mesh_obj, protocol_obj)
+    eit.setup(weight="none")
+    # the normalize for BP when dist_exc>4 should always be True
+    ds = 192.0 * eit.solve(v1, v0, normalize=True)
+
+    # extract node, element, alpha
+    pts = mesh_obj.node
+    tri = mesh_obj.element
+
+    # draw
+    fig, axes = plt.subplots(1, 1, constrained_layout=True)
+    if path1_for_name_only is None or path2_for_name_only is None:
+        plt.title("BP solver")
+    else:
+        name1 = path1_for_name_only.split("\\")[-1]
+        name2 = path2_for_name_only.split("\\")[-1]
+        plt.title(f"{name1} - {name2}")
+    # original
+    axes.axis("equal")
+    axes.set_title(r"Input $\Delta$ Conductivities")
+    # reconstructed
+    im = axes.tripcolor(pts[:, 0], pts[:, 1], tri, ds)
+    axes.set_title(r"Reconstituted $\Delta$ Conductivities")
+    axes.axis("equal")
+    fig.colorbar(im, ax=axes)
+    # fig.savefig('../doc/images/demo_bp.png', dpi=96)
+    plt.show()
--- time_diff_eit_test.py
+++ time_diff_eit_test.py
@@ -1,24 +1,23 @@
 import time
 
 import numpy as np
-import pandas as pd
 import torch
 from matplotlib import pyplot as plt
 
-from Model_Training.Models import LinearModelWithDropout, LinearModel
+from Model_Training.Models import LinearModel
 from ScioSpec_EIT_Device.data_reader import convert_single_frequency_eit_file_to_df, convert_multi_frequency_eit_to_df
 from Model_Training.model_plot_utils import plot_single_reconstruction
 from pyeit import mesh
-from pyeit.eit import protocol, jac, greit, bp
-from pyeit.eit.interp2d import sim2pts
+from pyeit.eit import protocol
 from pyeit.mesh.shape import thorax
 import os
 
+from reconstruction_algorithims import solve_and_plot_jack, solve_and_plot_greit, solve_and_plot_bp
 from utils import wait_for_start_of_measurement, get_relevant_voltages
 
 """ 0. build mesh """
 n_el = 32  # nb of electrodes
-protocol_obj = protocol.create(n_el, dist_exc=1, step_meas=1, parser_meas="std")
+protocol_obj = protocol.create(n_el, dist_exc=8, step_meas=1, parser_meas="std")
 # Dist_exc is the distance between the excitation and measurement electrodes (in number of electrodes)
 
 use_customize_shape = False
@@ -33,12 +32,6 @@ anomaly = []
 mesh_new = mesh.set_perm(mesh_obj, anomaly=anomaly, background=1.0)
 delta_perm = np.real(mesh_new.perm - mesh_obj.perm)
 
-# extract node, element, alpha
-pts = mesh_obj.node
-# pts is the list of the nodes of the mesh (with coordinates)
-tri = mesh_obj.element
-# tri is the list of the elements of the mesh (with the nodes that compose them)
-x, y = pts[:, 0], pts[:, 1]
 
 
 
@@ -70,7 +63,7 @@ def plot_time_diff_eit_image(path1, path2, frequency=1000):
     v1 = get_relevant_voltages(df2_old, protocol_obj)
     difference = v1 - v0
     plt.plot(difference)
-    plt.title("Difference between two images")
+    plt.title("v1 - v0")
     # plt.plot(v1)
     # # plt.plot(v0)
     # plt.title("Voltages")
@@ -78,10 +71,10 @@ def plot_time_diff_eit_image(path1, path2, frequency=1000):
     img_name = path1.split('\\')[-1]
     save_path_cnn = f"{img_name}_cnn.png"
     save_path_jac = f"{img_name}_jac.png"
-    solve_and_plot_jack(path1, path2, v0, v1)
-    # solve_and_plot_greit(path1, path2, v0, v1)
-    # solve_and_plot_bp(path1, path2, v0, v1)
-    # solve_and_plot_stack(path1, path2, v0, v1)
+    solve_and_plot_jack(v0, v1, mesh_obj, protocol_obj, path1_for_name_only=path1, path2_for_name_only=path2)
+    solve_and_plot_greit(v0, v1, mesh_obj, protocol_obj, path1_for_name_only=path1, path2_for_name_only=path2)
+    solve_and_plot_bp(v0, v1, mesh_obj, protocol_obj, path1_for_name_only=path1, path2_for_name_only=path2)
+    solve_and_plot_cnn(model=model, voltage_difference=difference)
     # solve_and_plot_cnn(model=model, voltage_difference=difference)
     # time.sleep(0.5)
 
@@ -104,154 +97,7 @@ def plot_frequencies_diff_eit_image(path, f1,f2):
     v0 = get_relevant_voltages(df1, protocol_obj)
     v1 = get_relevant_voltages(df2, protocol_obj)
 
-    solve_and_plot_jack(f"{f1}", f"{f2}", v0, v1)
-
-def solve_and_plot_greit(path1, path2, v0, v1):
-    """ 3. Construct using GREIT """
-    eit = greit.GREIT(mesh_obj, protocol_obj)
-    eit.setup(p=0.50, lamb=0.01, perm=1, jac_normalized=True)
-    ds = eit.solve(v1, v0, normalize=True)
-    x, y, ds = eit.mask_value(ds, mask_value=np.NAN)
-
-    # show alpha
-    fig, axes = plt.subplots(2, 1, constrained_layout=True, figsize=(6, 9))
-    name1 = path1.split("\\")[-1]
-    name2 = path2.split("\\")[-1]
-    plt.title(f"{name1} - {name2}")
-
-    ax = axes[0]
-    im = ax.tripcolor(pts[:, 0], pts[:, 1], tri, delta_perm, shading="flat")
-    ax.axis("equal")
-    ax.set_xlim([-1.2, 1.2])
-    ax.set_ylim([-1.2, 1.2])
-    ax.set_title(r"$\Delta$ Conductivity")
-    # fig.set_size_inches(6, 4)
-
-    # plot
-    ax = axes[1]
-    im = ax.imshow(np.real(ds), interpolation="none", cmap=plt.cm.viridis)
-    ax.axis("equal")
-
-    fig.colorbar(im, ax=axes.ravel().tolist())
-    # fig.savefig('../doc/images/demo_greit.png', dpi=96)
-    plt.show()
-
-
-def solve_and_plot_jack(path1, path2, v0, v1, save_path=None):
-    """ 3. JAC solver """
-    # Note: if the jac and the real-problem are generated using the same mesh,
-    # then, data normalization in solve are not needed.
-    # However, when you generate jac from a known mesh, but in real-problem
-    # (mostly) the shape and the electrode positions are not exactly the same
-    # as in mesh generating the jac, then data must be normalized.
-    eit = jac.JAC(mesh_obj, protocol_obj)
-    eit.setup(p=0.5, lamb=0.01, method="kotre", perm=1, jac_normalized=True)
-    start_time = time.time()
-    ds = eit.solve(v1, v0, normalize=True)
-    # print(f"JAC took {(time.time() - start_time)*1000} ms")
-    # ds is the delta sigma, the difference between the permittivity with and without anomaly
-    # A list of 704 values
-    # pts is the list of the nodes of the mesh
-    # tri is the list of the elements of the mesh (triangles denote connectivity [[i, j, k]])
-    ds_n = sim2pts(pts, tri, np.real(ds))
-    # ds_n is the delta sigma interpolated on the mesh nodes
-    # plot ground truth
-    fig, axes = plt.subplots(1, 2, constrained_layout=True)
-    name1 = path1.split("\\")[-1]
-    name2 = path2.split("\\")[-1]
-    plt.title(f"{name1} - {name2}")
-    fig.set_size_inches(9, 4)
-    ax = axes[0]
-    delta_perm = mesh_new.perm - mesh_obj.perm
-    # delta_perm is the difference between the permittivity with and without anomaly
-    # ( The same what we want to reconstruct )
-    im = ax.tripcolor(x, y, tri, np.real(delta_perm), shading="flat")
-    # Create a pseudocolor plot of an unstructured triangular grid.
-    ax.set_aspect("equal")
-    # plot EIT reconstruction
-    ax = axes[1]
-    im = ax.tripcolor(x, y, tri, ds_n, shading="flat")
-    for i, e in enumerate(mesh_obj.el_pos):
-        ax.annotate(str(i + 1), xy=(x[e], y[e]), color="r")
-    ax.set_aspect("equal")
-    fig.colorbar(im, ax=axes.ravel().tolist())
-    if save_path:
-        fig.savefig(save_path, dpi=96)
-    plt.show()
-    # plt.draw()    # For non Pycharm Plotting
-    # plt.pause(0.1)
-    # # close all figures to display the next one
-    # print("done")
-
-
-def solve_and_plot_bp(path1, path2, v0, v1):
-    """ 3. naive inverse solver using back-projection """
-    eit = bp.BP(mesh_obj, protocol_obj)
-    eit.setup(weight="none")
-    # the normalize for BP when dist_exc>4 should always be True
-    ds = 192.0 * eit.solve(v1, v0, normalize=True)
-
-    # extract node, element, alpha
-    pts = mesh_obj.node
-    tri = mesh_obj.element
-
-    # draw
-    fig, axes = plt.subplots(2, 1, constrained_layout=True, figsize=(6, 9))
-    name1 = path1.split("\\")[-1]
-    name2 = path2.split("\\")[-1]
-    plt.title(f"{name1} - {name2}")
-    # original
-    ax = axes[0]
-    ax.axis("equal")
-    ax.set_title(r"Input $\Delta$ Conductivities")
-    delta_perm = np.real(mesh_new.perm - mesh_obj.perm)
-    im = ax.tripcolor(pts[:, 0], pts[:, 1], tri, delta_perm, shading="flat")
-    # reconstructed
-    ax1 = axes[1]
-    im = ax1.tripcolor(pts[:, 0], pts[:, 1], tri, ds)
-    ax1.set_title(r"Reconstituted $\Delta$ Conductivities")
-    ax1.axis("equal")
-    fig.colorbar(im, ax=axes.ravel().tolist())
-    # fig.savefig('../doc/images/demo_bp.png', dpi=96)
-    plt.show()
-
-def solve_and_plot_stack(path1, path2, v0, v1):
-    """ 3. solving using dynamic EIT """
-    # number of stimulation lines/patterns
-    eit = jac.JAC(mesh_obj, protocol_obj)
-    eit.setup(p=0.40, lamb=1e-3, method="kotre", jac_normalized=False)
-    ds = eit.solve(v1, v0, normalize=False)
-
-    # extract node, element, alpha
-    pts = mesh_obj.node
-    tri = mesh_obj.element
-    delta_perm = mesh_new.perm - mesh_obj.perm
-
-    # # show alpha
-    # fig, ax = plt.subplots(figsize=(6, 4))
-    # im = ax.tripcolor(pts[:, 0], pts[:, 1], tri, np.real(delta_perm), shading="flat")
-    # fig.colorbar(im)
-    # ax.set_aspect("equal")
-    # ax.set_title(r"$\Delta$ Permittivity")
-
-    """ 4. plot """
-    fig, ax = plt.subplots(figsize=(6, 4))
-    im = ax.tripcolor(
-        pts[:, 0],
-        pts[:, 1],
-        tri,
-        np.real(ds),
-        shading="flat",
-        alpha=0.90,
-        cmap=plt.cm.viridis,
-    )
-    fig.colorbar(im)
-    ax.set_aspect("equal")
-    ax.set_title(r"$\Delta$ Permittivity Reconstructed")
-    # plt.savefig('quasi-demo-eit.pdf')
-    plt.show()
-
-
+    solve_and_plot_jack(v0, v1, mesh_obj, protocol_obj, path1_for_name_only=f"{f1}", path2_for_name_only=f"{f2}")
 
 
 def plot_eit_images_in_folder(path):
