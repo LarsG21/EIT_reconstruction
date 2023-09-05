71afc414923af03a95a019086719a74f2918cd01
Lars_Labor_PC <larsgudjons@gmail.com>
2023-09-05 12:34:15 +0200

    Added plotting of model.

---
+++ Model_Training/model_plot.py
@@ -0,0 +1,20 @@
+import torch
+from matplotlib import pyplot as plt
+from torchviz import make_dot
+
+from Models import LinearModelWithDropout
+
+VOLTAGE_VECTOR_LENGTH = 1024
+OUT_SIZE = 64
+# Visualize the model
+
+model = LinearModelWithDropout(input_size=VOLTAGE_VECTOR_LENGTH, output_size=OUT_SIZE ** 2)
+model.load_state_dict(torch.load(
+    "../Collected_Data/Combined_dataset/Models/LinearModelDropout/30_08_with_noise_and_rotation_augmentation/model_2023-08-30_14-15-52_200_epochs.pth"))
+
+x = torch.randn(1, 1024)
+y = model(x)
+make_dot(y, params=dict(list(model.named_parameters()) + [('x', x)]), show_attrs=False, show_saved=True).render(
+    "Linear_Dropout", format="png")
+
+torch.onnx.export(model, x, "model.onnx")
