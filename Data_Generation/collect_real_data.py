453f548405eceac45f1f550e4751517ed8e45831
Lars_Labor_PC <larsgudjons@gmail.com>
2023-09-21 12:52:03 +0200

    Collected new samples.

--- Data_Generation/collect_real_data.py
+++ Data_Generation/collect_real_data.py
@@ -229,11 +229,11 @@ def main():
     for device in devices:
         if "USB-SERIAL CH340" in device.description:
             ender = GCodeDevice(device.device, movement_speed=6000,
-                                home_on_init=False
+                                # home_on_init=False
                                 )
             MAX_RADIUS = RADIUS_TANK_IN_MM
             ender.maximal_limits = [MAX_RADIUS, MAX_RADIUS, MAX_RADIUS]
-            # calibration_procedure(ender)
+            calibration_procedure(ender)
             break
     if ender is None:
         if COLLECT_NEGATIVE_SAMPLES:
@@ -243,7 +243,7 @@ def main():
             raise Exception("No Ender 3 found")
     else:
         print("Ender 3 found")
-    TEST_NAME = "Data_14_09_40mm_multifreq"
+    TEST_NAME = "Data_21_09_40mm_multifreq"
     collect_data(gcode_device=ender, number_of_samples=4000,
                  eit_data_path="../eit_data",
                  save_path=f"C:/Users/lgudjons/PycharmProjects/EIT_reconstruction/Collected_Data/{TEST_NAME}")
