63a55d168151ef840da74cefff75ad1297f0b5ce
Lars_Labor_PC <larsgudjons@gmail.com>
2023-09-21 14:11:02 +0200

    Added plotting of principal components. Analysed Relevance of different principal components.

--- PCA.py
+++ PCA.py
@@ -5,55 +5,81 @@ import pandas as pd
 from sklearn.decomposition import PCA
 import numpy as np
 
-df = pd.read_pickle("Collected_Data/Combined_dataset_multi_augmented/augmented_data.pkl")
-v0 = np.load("Collected_Data/Combined_dataset/v0.npy")
 
-MULTI_FREQUENCY_EIT = True
+# df = pd.read_pickle("Collected_Data/Combined_dataset_multi_augmented/augmented_data.pkl")
+# v0 = np.load("Collected_Data/Combined_dataset/v0.npy")
+# #
+# MULTI_FREQUENCY_EIT = True
+#
+# def reduce_voltages_with_pca(df: pd.DataFrame, save_path: str, n_components=1024):
+#     """
+#     Reduces the voltages with pca.
+#     :param df:
+#     :return:
+#     """
+#     df = df[df["voltages"].apply(lambda x: len(x) == 20480)]
+#     voltages = df["voltages"].to_list()
+#     # print len of the voltages in the list
+#     # for v in voltages:
+#     #     print(len(v))
+#     voltages_array = np.array(voltages)
+#     if not MULTI_FREQUENCY_EIT:
+#         voltages_array = (voltages_array - v0) / v0  # normalized voltage difference
+#         voltages_array = voltages_array - np.mean(voltages_array)
+#     # do pca on voltages
+#     pca = PCA(n_components=n_components)
+#     pca.fit(voltages_array)
+#     voltages_pca = pca.transform(voltages_array)
+#     # save pca model
+#     if not os.path.exists(os.path.dirname(save_path)):
+#         os.makedirs(os.path.dirname(save_path))
+#     with open(os.path.join(os.path.dirname(save_path), "pca.pkl"), "wb") as f:
+#         pickle.dump(pca, f)
+#
+#     # plot the main components
+#     import matplotlib.pyplot as plt
+#     plt.scatter(voltages_pca[:, 0], voltages_pca[:, 1])
+#     plt.xlabel("PC1")
+#     plt.ylabel("PC2")
+#     plt.show()
+#     # get variance of pc1 and pc2 ...
+#     for i in range(10):
+#         print(f"Variance of PC{i}: {np.var(voltages_pca[:, i])}")
+#     # copy df
+#     df_pca = df.copy()
+#     df_pca["voltages"] = voltages_pca.tolist()
+#     print(df_pca.head())
+#     # save df
+#     df_pca.to_pickle(save_path)
 
-def reduce_voltages_with_pca(df: pd.DataFrame, save_path: str, n_components=1024):
+
+# n_components = 128
+#
+# reduce_voltages_with_pca(df=df,
+#                          save_path=f"Collected_Data/Combined_dataset_multi_augmented/PCA_REDUCED{n_components}/combined_pca.pkl",
+#                          n_components=n_components)
+
+def get_main_parts_of_principal_components(pca):
     """
-    Reduces the voltages with pca.
-    :param df:
+
+    :param pca:
     :return:
     """
-    df = df[df["voltages"].apply(lambda x: len(x) == 20480)]
-    voltages = df["voltages"].to_list()
-    # print len of the voltages in the list
-    # for v in voltages:
-    #     print(len(v))
-    voltages_array = np.array(voltages)
-    if not MULTI_FREQUENCY_EIT:
-        voltages_array = (voltages_array - v0) / v0  # normalized voltage difference
-        voltages_array = voltages_array - np.mean(voltages_array)
-    # do pca on voltages
-    pca = PCA(n_components=n_components)
-    pca.fit(voltages_array)
-    voltages_pca = pca.transform(voltages_array)
-    # save pca model
-    if not os.path.exists(os.path.dirname(save_path)):
-        os.makedirs(os.path.dirname(save_path))
-    with open(os.path.join(os.path.dirname(save_path), "pca.pkl"), "wb") as f:
-        pickle.dump(pca, f)
-
-    # plot the main components
+    components = pca.components_
+    print("Hauptkomponenten:")
+    print(components)
+    # plot the main components as image
     import matplotlib.pyplot as plt
-    plt.scatter(voltages_pca[:, 0], voltages_pca[:, 1])
-    plt.xlabel("PC1")
-    plt.ylabel("PC2")
+    plt.imshow(components, cmap="gray")
     plt.show()
-    # get variance of pc1 and pc2 ...
     for i in range(10):
-        print(f"Variance of PC{i}: {np.var(voltages_pca[:, i])}")
-    # copy df
-    df_pca = df.copy()
-    df_pca["voltages"] = voltages_pca.tolist()
-    print(df_pca.head())
-    # save df
-    df_pca.to_pickle(save_path)
+        print(f"Variance of PC{i}: {np.var(components[i])}")
+        plt.plot(components[i])
+        plt.show()
 
 
-n_components = 128
-
-reduce_voltages_with_pca(df=df,
-                         save_path=f"Collected_Data/Combined_dataset_multi_augmented/PCA_REDUCED{n_components}/combined_pca.pkl",
-                         n_components=n_components)
+model_pca_path = "Collectad_Data_Experiments/How_many_frequencies_are_needet_for_abolute_EIT/10_Frequencies/Models/LinearModelWithDropout/run_1_with_augmentation_pca_reduced/model_2023-09-21_13-02-00_200_epochs.pth"
+# get the pca.okl in the same folder as the model
+pca_path = os.path.join(os.path.dirname(model_pca_path), "pca.pkl")
+pca = pickle.load(open(pca_path, "rb"))
+get_main_parts_of_principal_components(pca)
--- live_eit_reconstruction_multi_freq.py
+++ live_eit_reconstruction_multi_freq.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import time
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -12,16 +13,24 @@ from plot_utils import solve_and_plot_cnn
 from utils import wait_for_start_of_measurement
 
 
-def plot_multi_frequency_eit_image(v1_path):
+def plot_multi_frequency_eit_image(v1_path, plot=False):
     global default_frame
     df = convert_multi_frequency_eit_to_df(v1_path)
     df_alternating = pd.DataFrame({"real": df["real"], "imaginary": df["imaginary"]}).stack().reset_index(drop=True)
     df_alternating = df_alternating.to_frame(name="amplitude")
     v1 = df_alternating["amplitude"].to_numpy(dtype=np.float64)
+    # plt.plot(v1)
+    # plt.show()
     # save v0 as npy
     PCA = True
     if PCA:
         v1 = pca.transform(v1.reshape(1, -1))
+        if plot:
+            plt.bar(x=range(len(v1.reshape(-1))), height=v1.reshape(-1))
+            plt.title("PCA transformed voltage vector")
+            plt.xlabel("PCA component")
+            plt.ylabel("Intensity")
+            plt.show()
     solve_and_plot_cnn(model=model_pca, voltage_difference=v1, chow_center_of_mass=True)
 
 
@@ -43,7 +52,7 @@ def plot_eit_video(path):
                 seen_files.append(current_frame)
 
 
-path = "eit_data"
+path = "live_plot"
 
 VOLTAGE_VECTOR_LENGTH = 1024
 VOLTAGE_VECTOR_LENGTH_PCA = 128
