57efbc92923fd32fcbc71f368028c6040b8cce56
Lars_Labor_PC <larsgudjons@gmail.com>
2023-10-27 16:25:51 +0200

    Implemented generation of greit noise figure sample data

---
+++ Data_Generation/generate_noise_figure_data.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+VOLTAGE_VECTOR_LENGTH = 1024
+OUT_SIZE = 64
+
+
+def generate_noise_figure_data():
+    voltages = []
+    images = []
+    for v in range(VOLTAGE_VECTOR_LENGTH):
+        voltage_vector = np.zeros(VOLTAGE_VECTOR_LENGTH)
+        voltage_vector[v] = 0.1
+        voltages.append(voltage_vector)
+        images.append(np.zeros([OUT_SIZE, OUT_SIZE]))
+    # save as df
+    df = pd.DataFrame()
+    df["voltages"] = voltages
+    df["images"] = images
+    df.to_pickle("noise_figure_data.pkl")
+
+
+if __name__ == '__main__':
+    generate_noise_figure_data()
