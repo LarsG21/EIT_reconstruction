3ce71f388f93aa6279f0d0f96793262a1a191fa4
Lars_Labor_PC <larsgudjons@gmail.com>
2023-12-11 11:57:25 +0100

    Added averaging of V0 EIT Frame to be more noise free.

--- live_eit_reconstruction_time_diff.py
+++ live_eit_reconstruction_time_diff.py
@@ -32,12 +32,49 @@ delta_perm = np.real(mesh_new.perm - mesh_obj.perm)
 
 default_frame = None
 
+list_for_default_frames_to_average = []
+
+
+def get_averaged_frame(path, number_of_avgs):
+    """
+    Gets the averaged frame from the given path. This is used as the default frame for the time difference.
+    :param path:
+    :param number_of_avgs:
+    :return:
+    """
+    global default_frame
+    global list_for_default_frames_to_average
+    df = convert_multi_frequency_eit_to_df(path)
+    df = df[df["frequency"] == 1000]
+    v1 = df["amplitude"].to_numpy(dtype=np.float64)
+    if default_frame is None:
+        list_for_default_frames_to_average.append(v1)
+        print(f"Added frame to list {path}")
+        if len(list_for_default_frames_to_average) >= number_of_avgs:
+            temp = np.mean(np.array(list_for_default_frames_to_average), axis=0)
+            # list_for_default_frames_to_average = []
+            temp = temp.reshape(-1)
+            print("Default frame calculated")
+            # convert to df with the same format as the other frames
+            df = df[df["frequency"] == 1000]
+            df["amplitude"] = temp
+            default_frame = df
+
 
 def plot_time_diff_eit_image(v1_path, v0_path, debug_plots=False):
+    """
+    Plots the time difference between the two given eit frames.
+    :param v1_path:
+    :param v0_path:
+    :param debug_plots:
+    :return:
+    """
     global default_frame
     df_v1 = convert_multi_frequency_eit_to_df(v1_path)
     if default_frame is None:
-        df_v0 = convert_multi_frequency_eit_to_df(v0_path)
+        # df_v0 = convert_multi_frequency_eit_to_df(v0_path)
+        get_averaged_frame(v1_path, number_of_avgs=30)
+        return
     else:
         df_v0 = default_frame
     df_v1 = df_v1[df_v1["frequency"] == 1000]
@@ -81,29 +118,6 @@ def plot_time_diff_eit_image(v1_path, v0_path, debug_plots=False):
                                       use_opencv_for_plotting=True)
 
 
-def plot_eit_images_in_folder(path):
-    old_path = os.getcwd()
-    eit_path = ""
-    for file_or_folder in os.listdir(path):
-        if os.path.isdir(os.path.join(path, file_or_folder)):
-            os.chdir(os.path.join(path, file_or_folder))
-            print(os.getcwd())
-            os.chdir((os.path.join(os.getcwd(), "setup")))
-            eit_path = os.getcwd()
-            print(eit_path)
-            break
-
-    default_frame = None
-    for current_frame in os.listdir(os.getcwd()):
-        if current_frame.endswith(".eit"):
-            if default_frame is None:
-                default_frame = current_frame
-            else:
-                print(default_frame, current_frame)
-                plot_time_diff_eit_image(os.path.join(eit_path, current_frame), os.path.join(eit_path, default_frame))
-                # last_frame = current_frame
-    # reset path
-    os.chdir(old_path)
 
 
 def plot_eit_video(path):
@@ -137,11 +151,9 @@ def plot_eit_video(path):
                     # seen_files = []
 
 
-
 path = "C:\\Users\\lgudjons\\Desktop\\eit_data"
 VOLTAGE_VECTOR_LENGTH = 1024
 OUT_SIZE = 64
-
 
 # model_path = "Collected_Data/Combined_dataset/Models/LinearModelWithDropout2/TESTING_MORE_DATA_12_10/model_2023-10-12_11-55-44_epoche_232_of_300_best_model.pth"
 #
@@ -159,7 +171,6 @@ print("Loading the model")
 model = LinearModelWithDropout2(input_size=VOLTAGE_VECTOR_LENGTH, output_size=OUT_SIZE ** 2)
 model.load_state_dict(torch.load(model_path))
 model.eval()
-
 
 model.eval()
 plot_eit_video(path)
